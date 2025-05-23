383dd5dd9242d7e7236a355655ded9264da3728a
Suyash Shukla <shuklasuyash52@gmail.com>
2025-05-23 23:50:25 +0530

    j

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -7,18 +7,29 @@ import com.bike.plugins.configureMonitoring
 import com.bike.plugins.configureRouting
 import com.bike.plugins.configureSerialization
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.config.*
+import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    EngineMain.main(args)
 }
 
 fun Application.module() {
+    // Override from env vars if set, else null to fallback
+    val dbUrl = System.getenv("DB_URL")
+    val dbUser = System.getenv("DB_USER")
+    val dbPassword = System.getenv("DB_PASSWORD")
+
+    initDatabase(
+        url = dbUrl,
+        user = dbUser,
+        password = dbPassword
+    )
+
     configureSerialization()
     configureMonitoring()
     configureHTTP()
     configureRouting()
-    initDatabase()
     initFirerBaseAdmin()
 }
 
--- src/main/kotlin/infrastructure/database/DatabaseFactory.kt
+++ src/main/kotlin/infrastructure/database/DatabaseFactory.kt
@@ -9,18 +9,24 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
-fun Application.initDatabase() {
+fun Application.initDatabase(
+    url: String? = null,
+    driver: String? = null,
+    user: String? = null,
+    password: String? = null
+) {
     val config = environment.config.config("database")
-    val url = config.property("url").getString()
-    val driver = config.property("driver").getString()
-    val user = config.property("user").getString()
-    val password = config.property("password").getString()
+
+    val dbUrl = url ?: config.property("url").getString()
+    val dbDriver = driver ?: config.property("driver").getString()
+    val dbUser = user ?: config.property("user").getString()
+    val dbPassword = password ?: config.property("password").getString()
 
     val hikariConfig = HikariConfig().apply {
-        jdbcUrl = url
-        driverClassName = driver
-        username = user
-        this.password = password
+        jdbcUrl = dbUrl
+        driverClassName = dbDriver
+        username = dbUser
+        this.password = dbPassword
         maximumPoolSize = 3
         isAutoCommit = false
         transactionIsolation = "TRANSACTION_REPEATABLE_READ"
@@ -29,6 +35,6 @@ fun Application.initDatabase() {
     val dataSource = HikariDataSource(hikariConfig)
     Database.connect(dataSource)
     transaction {
-        SchemaUtils.create(UserTable,BikesTable)
+        SchemaUtils.create(UserTable, BikesTable)
     }
 }
